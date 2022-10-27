34e6ad32693c0327cda3f354893b0c04d4e47821
kalleleander <65467762+kalleleander@users.noreply.github.com>
2022-10-27 20:14:40 +0200

    Update Intelli.py

--- Intelli.py
+++ Intelli.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import numpy as np
+import pandas as pd
+from numpy import unique
+from numpy import where
+from matplotlib import pyplot
+from sklearn.datasets import make_classification
+from sklearn.cluster import KMeans
 
-print("Hello World!")
-df = data
+# initialize the data set we'll work with
+training_data = pd.read_csv('file_name.csv')
 
-Poul er en luder
+# define the model
+kmeans_model = KMeans(n_clusters=2)
+
+# assign each data point to a cluster
+dbscan_result = dbscan_model.fit_predict(training_data)
+
+# get all of the unique clusters
+dbscan_clusters = unique(dbscan_result)
+
+# plot the DBSCAN clusters
+for dbscan_cluster in dbscan_clusters:
+    # get data points that fall in this cluster
+    index = where(dbscan_result == dbscan_clusters)
+    # make the plot
+    pyplot.scatter(training_data[index, 0], training_data[index, 1])
+
+# show the DBSCAN plot
+pyplot.show()
